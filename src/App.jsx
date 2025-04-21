39775a3875835b7559e30ec497c22a88d188459d
onnik <onni.kivinen@gmail.com>
2025-04-21 17:39:42 +0300

    App rungon alustaminen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css';
 
+import {BrowserRouter, Route, Routes} from 'react-router';
+import Layout from './components/Layout';
+import Home from './views/Home';
+import Menu from './views/Menu';
+import About from './views/About';
+import Login from './views/Login';
+import Reservation from './views/Reservation';
+import Register from './views/Register';
+
 function App() {
   return (
-    <>
-      <h1> My App </h1>
-    </>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
