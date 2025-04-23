f6acbdd7cdca249d115bbc3ab5b151b78e531de1
TonyKarlin <tony.karlin@metropolia.fi>
2025-04-23 14:51:05 +0300

    docs: update comments for i18n usage instructions

--- src/assets/i18n.jsx
+++ src/assets/i18n.jsx
@@ -1,6 +1,13 @@
 import i18n from 'i18next'
 import  {initReactI18next} from 'react-i18next'
 
+// HOW TO USE: Use the useTranslation hook in your components
+// import { useTranslation } from 'react-i18next';
+// const { t } = useTranslation();
+// t('homePage.welcome-message') // This will return the translated string based on the current language
+// add translations under key 'homePage' (in this example) in the resources object
+// with key-value pair 'welcome-message': 'translation string' for each language
+
 i18n.use(initReactI18next).init({
   debug: true,
   fallbackLng: 'en',
