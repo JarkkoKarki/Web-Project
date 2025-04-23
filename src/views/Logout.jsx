768cdb7f61d9830f5993b2e881383b90b9c4986b
onnik <onni.kivinen@gmail.com>
2025-04-23 18:55:09 +0300

    feat: refactor Logout component to use useEffect for handleLogout

--- src/views/Logout.jsx
+++ src/views/Logout.jsx
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 import { useUserContext } from "../components/hooks/contextHooks";
 
 
 const Logout = () => {
-  const {handleLogout} = useUserContext();
-  handleLogout();
-  return <p>Logout page here</p>;
+  const { handleLogout } = useUserContext();
+
+  useEffect(() => {
+    handleLogout(); // Call handleLogout after the component has mounted
+  }, [handleLogout]);
+
+  return <p>Logging out...</p>; // Optional: Show a message or redirect
 };
 
 export default Logout;
