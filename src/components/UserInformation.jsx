d682e1a4269d2ed0597c8a43eb567d4d9cb50718
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2025-04-24 10:53:49 +0300

    refactor: enhance UserInformation component for better interactivity and readability

--- src/components/UserInformation.jsx
+++ src/components/UserInformation.jsx
@@ -1,20 +1,41 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import React from 'react'
-
 export const UserInformation = () => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
+
+  const clickHandler = (field) => {
+    console.log(`${field} clicked`);
+  };
+
+
+  const profileInfo = [
+    { label: t('profilePage.name'), value: 'John Doe' },
+    { label: t('profilePage.username'), value: 'Johnny' },
+    { label: t('profilePage.email'), value: 'Johnny@test.com' },
+    { label: t('profilePage.address'), value: 'gloverstreet203' },
+    { label: t('profilePage.phone-number'), value: '0443452340' },
+  ];
+
   return (
     <div className='flex flex-col items-start space-y-4 w-1/3 h-full'>
-        <h3 className='self-start font-bold text-2xl mb-8'>{t("profilePage.user-information")}</h3>
-          <p className='text-xl bg-[#101211] rounded border-[#000000] p-2'>{t("profilePage.name")}</p>
-          <p className='text-xl bg-[#101211] rounded border-[#101211] p-2'>{t("profilePage.username")}</p>
-          <p className='text-xl bg-[#101211] rounded border-[#101211] p-2'>{t("profilePage.email")}</p>
-          <p className='text-xl bg-[#101211] rounded border-[#101211] p-2'>{t("profilePage.address")}</p>
-          <p className='text-xl bg-[#101211] rounded border-[#101211] p-2'>{t("profilePage.phone-number")}</p>
+      <h3 className='self-start font-bold text-2xl mb-8'>{t('profilePage.user-information')}</h3>
+      {profileInfo.map((info, index) => (
+        <div
+          key={index}
+          className='w-full text-xl bg-[#101211] rounded border-[#000000] p-2 cursor-pointer hover:bg-[#1c1e24] hover:border-[#000000] border-2 flex justify-between'
+          onClick={() => clickHandler(info.label)}
+        >
+          <span>{info.label}</span>
+          <span className='flex items-center space-x-2'>
+            <span>{info.value}</span>
+            <span className='font-bold'>{">"}</span>
+          </span>
         </div>
-  )
-}
+      ))}
+    </div>
+  );
+};
 
-export default UserInformation
+export default UserInformation;
 
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = () => {
   return (
     <div className='flex flex-col items-center justify-center bg-[#0d0f0e] text-white font-sans'>
       <h2 className='mb-12 text-3xl font-bold'>{t("profilePage.profile")}</h2>
-      <section className='flex flex-row items-center justify-center space-x-20 px-6 max-w-6xl h-[400px]'>
+      <section className='flex flex-row items-center justify-center space-x-20 px-6 w-full h-[400px]'>
       <ProfilePicture />
       <UserInformation />
       </section>
