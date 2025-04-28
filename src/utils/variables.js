d6a5ece5b8aeb06ba25ffeebcb353dfbe25f0eb7
onnik <onni.kivinen@gmail.com>
2025-04-28 17:58:28 +0300

    fix: correct formatting of URL constants in variables.js

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,2 +1,4 @@
-export const url = "http://10.120.32.87/app/api"
-export const rootUrl = "http://10.120.32.87/app/"
+export const url = 'http://10.120.32.87/app/api';
+export const rootUrl = 'http://10.120.32.87/app/';
+export const logoUrl =
+  'http://10.120.32.87/app/uploads/fbca0b6586708fbc2cfe7645a7efe9b2_thumb.jpg';
