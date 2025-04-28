48e48d3fbd36223d6d974c15a8fd35a863045b6c
onnik <onni.kivinen@gmail.com>
2025-04-28 18:40:32 +0300

    feat: integrate toast notifications for add to cart functionality in MenuItem component

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,60 @@ import Menu from './views/Menu';
 import About from './views/About';
 import Login from './views/Login';
 import Reservation from './views/Reservation';
-import "./assets/i18n";
-import { UserProvider } from './contexts/UserContext';
+import './assets/i18n';
+import {UserProvider} from './contexts/UserContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Profile from './views/Profile';
 import Logout from './views/Logout';
 import WorkHub from './views/WorkHub';
-import { LanguageProvider } from './contexts/LanguageContext';
-
+import {LanguageProvider} from './contexts/LanguageContext';
+import {ToastContainer} from 'react-toastify';
 
 function App() {
-
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <LanguageProvider>
-      <UserProvider>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/workhub/*"
-              element={
-                <ProtectedRoute roles={['admin', 'employee']}>
-                  <WorkHub />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/reservation" element={<Reservation />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/" element={<Home />} />
-          </Route>
-      </Routes>
-      </UserProvider>
+        {/* ToastContainer for global toast notifications */}
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <UserProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/workhub/*"
+                element={
+                  <ProtectedRoute roles={['admin', 'employee']}>
+                    <WorkHub />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/reservation" element={<Reservation />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/" element={<Home />} />
+            </Route>
+          </Routes>
+        </UserProvider>
       </LanguageProvider>
     </BrowserRouter>
   );
--- src/components/MenuRowItem.jsx
+++ src/components/MenuRowItem.jsx
@@ -1,6 +1,18 @@
 import {toUpper} from 'lodash';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MenuItem = ({item, addToCart}) => {
+  const handleAddToCart = () => {
+    if (addToCart) {
+      addToCart(item);
+      toast.success(`${item.name} added to cart!`, {
+        position: 'top-right',
+        autoClose: 2000,
+      });
+    }
+  };
+
   return (
     <div className="menu-item flex items-center justify-between border-b-1 border-b-gray-800">
       <div className="flex flex-col">
@@ -11,7 +23,7 @@ const MenuItem = ({item, addToCart}) => {
       {addToCart && (
         <button
           className="ml-4 cursor-pointer rounded bg-yellow-400 px-2 py-1 text-sm text-black hover:bg-emerald-600"
-          onClick={() => addToCart(item)}
+          onClick={handleAddToCart}
         >
           Add to Cart
         </button>
