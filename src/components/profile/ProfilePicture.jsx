9be24b78ba4732e0817de3f9d446da8efe9e12c9
TonyKarlin <tony.karlin@metropolia.fi>
2025-05-02 23:47:27 +0300

    refactor: update translation keys from 'change' to 'upload' in i18n and ProfilePicture components

--- src/components/profile/ProfilePicture.jsx
+++ src/components/profile/ProfilePicture.jsx
@@ -95,7 +95,7 @@ export const ProfilePicture = () => {
             type="submit"
             className='"mt-6 inline-block cursor-pointer border border-yellow-500 px-6 py-2 text-yellow-500 transition hover:bg-yellow-500 hover:text-black'
           >
-            {t('profilePage.change')}
+            {t('profilePage.upload')}
           </button>
           <input
             title="file"
