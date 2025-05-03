90ee86a893e38df87d79499e37182fb8c4a6ae2c
onnik <onni.kivinen@gmail.com>
2025-05-03 16:47:01 +0300

    refactor: conditionally render shopping cart based on user authentication

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -119,29 +119,33 @@ const Layout = () => {
       <main className="p-8">
         <Outlet />
       </main>
-      {/* Shopping Cart Button */}
-      {(location.pathname === '/' ||
-        location.pathname === '/menu' ||
-        location.pathname === '/about') && (
-        <button
-          onClick={toggleCart}
-          className="fixed right-4 bottom-4 cursor-pointer rounded-full bg-yellow-500 p-4 text-black shadow-lg transition hover:bg-yellow-600"
-        >
-          🛒
-        </button>
-      )}
-      {/* Shopping Cart Sidebar */}
-      {isCartOpen && (
-        <div className="bottom-20% fixed top-20 right-10 z-999 h-auto w-100 bg-gray-800 p-4 text-white shadow-lg">
-          {' '}
-          <button
-            onClick={toggleCart}
-            className="cursor-pointer rounded-full bg-red-500 p-2 text-white hover:bg-red-600"
-          >
-            Close
-          </button>
-          <ShoppingCartElement />
-        </div>
+      {user && (
+        <>
+          {/* Shopping Cart Button */}
+          {(location.pathname === '/' ||
+            location.pathname === '/menu' ||
+            location.pathname === '/about') && (
+            <button
+              onClick={toggleCart}
+              className="fixed right-4 bottom-4 cursor-pointer rounded-full bg-yellow-500 p-4 text-black shadow-lg transition hover:bg-yellow-600"
+            >
+              🛒
+            </button>
+          )}
+
+          {/* Shopping Cart Sidebar */}
+          {isCartOpen && (
+            <div className="bottom-20% fixed top-20 right-10 z-999 h-auto w-100 bg-gray-800 p-4 text-white shadow-lg">
+              <button
+                onClick={toggleCart}
+                className="cursor-pointer rounded-full bg-red-500 p-2 text-white hover:bg-red-600"
+              >
+                Close
+              </button>
+              <ShoppingCartElement />
+            </div>
+          )}
+        </>
       )}
       <footer className="border-t border-gray-800 bg-[#0d0f0e] py-8 text-center text-sm text-gray-500">
         &copy; {t('footer.footer')}
