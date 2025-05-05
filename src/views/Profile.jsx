5b1225c737b6c0ad9399c1ddca350ff38127708b
TonyKarlin <tony.karlin@metropolia.fi>
2025-05-05 03:05:40 +0300

    feat: add logout link to Profile component for improved user navigation

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -4,6 +4,7 @@ import UserInformation from '../components/profile/UserInformation';
 import DeleteUserButton from '../components/profile/DeleteUserButton';
 import {OrderHistory} from '../components/orders/OrderHistory';
 import {useUserContext} from '../components/hooks/contextHooks';
+import {Link} from 'react-router';
 
 const Profile = () => {
   const {t} = useTranslation();
@@ -19,7 +20,13 @@ const Profile = () => {
       <section className="flex w-full flex-row items-center justify-center space-y-10 px-6">
         <OrderHistory />
       </section>
-      <section className="mt-10 flex h-auto w-full flex-row items-center justify-center space-x-20 px-6">
+      <section className="mt-10 flex h-auto w-full flex-row items-end justify-end space-x-10 px-20 py-10">
+        <Link
+          to="/logout"
+          className="mt-6 inline-block cursor-pointer border border-red-400 px-6 py-2 text-red-400 transition hover:bg-red-400 hover:text-black"
+        >
+          {t('header.sign-out')}
+        </Link>
         <DeleteUserButton userId={user.id} />
       </section>
     </div>
