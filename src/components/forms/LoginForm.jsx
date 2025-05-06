42f0c4cdcb0d6c40fbf6e118d405c5c2cdf7d10a
JarkkoKarki <karkijarkko@gmail.com>
2025-05-06 16:08:56 +0300

    style: update Login and Register forms for improved layout and styling

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -26,50 +26,52 @@ const LoginForm = () => {
 
   return (
     <>
-      <h2 className="mb-6 text-2xl font-bold">{t('loginPage.login')}</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label
-            htmlFor="loginuser"
-            className="mb-2 block text-left text-lg font-bold"
-          >
-            {t('loginPage.username')}
-          </label>
-          <input
-            onChange={handleInputChange}
-            type="text"
-            id="loginuser"
-            name="username"
-            placeholder={t('loginPage.username')}
-            autoComplete="username"
-            className="mb-5 w-full rounded-lg border border-gray-300 p-3 text-lg"
-          />
-        </div>
+      <div className="p-12">
+        <h2 className="mb-6 text-2xl font-bold">{t('loginPage.login')}</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="loginuser"
+              className="mb-3 block text-left text-lg font-bold"
+            >
+              {t('loginPage.username')}
+            </label>
+            <input
+              onChange={handleInputChange}
+              type="text"
+              id="loginuser"
+              name="username"
+              placeholder={t('loginPage.username')}
+              autoComplete="username"
+              className="mb-3 w-full rounded-lg border border-gray-300 p-3 text-lg"
+            />
+          </div>
 
-        <div>
-          <label
-            htmlFor="loginpassword"
-            className="mb-2 block text-left text-lg font-bold"
+          <div>
+            <label
+              htmlFor="loginpassword"
+              className="mb-3 block text-left text-lg font-bold"
+            >
+              {t('loginPage.password')}
+            </label>
+            <input
+              onChange={handleInputChange}
+              type="password"
+              id="loginpassword"
+              name="password"
+              placeholder={t('loginPage.password')}
+              autoComplete="current-password"
+              className="mb-3 w-full rounded-lg border border-gray-300 p-3 text-lg"
+            />
+          </div>
+          <button
+            type="submit"
+            className="mt-3 w-full rounded-lg bg-gray-400 p-3 text-lg font-bold text-white hover:bg-gray-500"
           >
-            {t('loginPage.password')}
-          </label>
-          <input
-            onChange={handleInputChange}
-            type="password"
-            id="loginpassword"
-            name="password"
-            placeholder={t('loginPage.password')}
-            autoComplete="current-password"
-            className="mb-5 w-full rounded-lg border border-gray-300 p-3 text-lg"
-          />
-        </div>
-        <button
-          type="submit"
-          className="mt-4 w-full rounded-lg bg-gray-400 p-3 text-lg font-bold text-white hover:bg-gray-500"
-        >
-          {t('loginPage.login')}
-        </button>
-      </form>
+            {t('loginPage.login')}
+          </button>
+        </form>
+      </div>
     </>
   );
 };
