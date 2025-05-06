fb45a5a6719fa53c6451ac05edfdd934da4b0957
onnik <onni.kivinen@gmail.com>
2025-05-07 01:58:40 +0300

    fix: update URLs in variables.js to use HTTPS

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,4 +1,4 @@
-export const url = '//10.120.32.87/app/api';
-export const rootUrl = '//10.120.32.87/app/';
+export const url = 'https://10.120.32.87/app/api';
+export const rootUrl = 'https://10.120.32.87/app/';
 export const logoUrl =
-  '//10.120.32.87/app/uploads/fbca0b6586708fbc2cfe7645a7efe9b2_thumb.jpg';
+  'https://10.120.32.87/app/uploads/fbca0b6586708fbc2cfe7645a7efe9b2_thumb.jpg';
