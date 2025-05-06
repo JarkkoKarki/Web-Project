91a70a26deccd76b9688e9cd094176deb8de4e72
TonyKarlin <tony.karlin@metropolia.fi>
2025-05-07 00:25:04 +0300

    refactor: improve layout and styling for Login, Register, and Profile components

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -21,13 +21,13 @@ const Login = () => {
 
   return (
     <div
-      className="flex h-screen items-center justify-center bg-cover bg-center bg-no-repeat"
+      className="flex h-screen flex-col items-center bg-cover bg-center bg-no-repeat md:justify-center"
       style={{backgroundImage: `url(${backgroundImage})`}}
     >
-      <div className="mx-auto mt-20 max-w-md rounded-md border border-gray-300 bg-[#0d0f0e] p-4 shadow-md">
+      <div className="mt-10 flex w-4/5 flex-col rounded-md border border-gray-300 bg-[#0d0f0e] p-4 shadow-md md:mt-0 md:max-w-md">
         {formToggle ? <LoginForm /> : <RegisterForm />}
         <button
-          className="mt-4 block text-lg font-bold text-blue-500 hover:underline"
+          className="mt-4 block self-start text-lg font-bold text-blue-500 hover:underline"
           onClick={clickHandler}
         >
           {formToggle ? t('loginPage.not-user') : t('loginPage.ready-user')}
